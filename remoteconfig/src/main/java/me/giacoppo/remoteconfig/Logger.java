5fff8d4706226e6371c9495a8d121c56b87d6397
Giuseppe Giacoppo <giuseppegiacoppo@hotmail.com>
2018-01-10 12:32:09 +0100

    visibility

--- remoteconfig/src/main/java/me/giacoppo/remoteconfig/Logger.java
+++ remoteconfig/src/main/java/me/giacoppo/remoteconfig/Logger.java
@@ -6,7 +6,7 @@ import android.util.Log;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-public final class Logger {
+final class Logger {
     private int logLevel;
 
     private Logger() {
@@ -17,13 +17,13 @@ public final class Logger {
         Holder.logger.logLevel = level;
     }
 
-    public static void log(@Level int logLevel, String message) {
+    static void log(@Level int logLevel, String message) {
         if (logLevel>=Holder.logger.logLevel)
             Log.d("RemoteConfig", message);
     }
 
-    public static final int RELEASE = 0x0;
-    public static final int DEBUG = 0x1;
+    static final int RELEASE = 0x0;
+    static final int DEBUG = 0x1;
 
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({DEBUG,RELEASE})
