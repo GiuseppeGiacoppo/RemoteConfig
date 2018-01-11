7c600205c7f55a8c17154373145d155dba7e42ea
Giuseppe Giacoppo <giuseppegiacoppo@hotmail.com>
2018-01-11 16:50:01 +0100

    clean

--- remoteconfig/src/main/java/me/giacoppo/remoteconfig/callback/SimpleCallback.java
+++ remoteconfig/src/main/java/me/giacoppo/remoteconfig/callback/SimpleCallback.java
@@ -1,12 +1,12 @@
 package me.giacoppo.remoteconfig.callback;
 
-import me.giacoppo.remoteconfig.RemoteConfig;
+import me.giacoppo.remoteconfig.RemoteResource;
 
 /**
  * Base callback
  */
 
-public abstract class SimpleCallback implements RemoteConfig.Callback {
+public abstract class SimpleCallback implements RemoteResource.Callback {
     @Override
     public void onError(Throwable t) {
         t.printStackTrace();
